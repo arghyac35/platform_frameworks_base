f91a77fc0c1aec22c7cb840c7aa0841f588aab72
LorDClockaN <davor@losinj.com>
2015-08-28 20:59:06 +0200

    Toast icon switch [1/2]
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: I24968141051805ee8c9bd5819e5f801c0dd78ef3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4721,6 +4721,12 @@ public final class Settings {
         public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
 
         /**
+         * Toast icon
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4922,6 +4928,7 @@ public final class Settings {
 	    PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_HIDEARROW);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_FONT_SIZE);
             PRIVATE_SETTINGS.add(SWAP_VOLUME_BUTTONS);
+            PRIVATE_SETTINGS.add(TOAST_ICON);
         }
 
         /**
