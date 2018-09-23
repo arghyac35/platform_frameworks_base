a3632103985db4634d792eae0ae9eecac4b1188e
ezio84 <brabus84@gmail.com>
2018-09-24 00:21:01 +0530

    Option for Up, Down as well for Network Traffic [1/2]
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: Ia7cc00301c1bf4d1d1c6ec7ec8eaafe3f6761e67

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4691,6 +4691,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
 
         /**
+	 ** What to show in network traffic indicator in statusbar
+	 ** @hide
+	 **/
+        public static final String NETWORK_TRAFFIC_TYPE = "network_traffic_type";
+
+        /**
          * Network traffic inactivity threshold (default is 1 kBs)
          * @hide
          */
@@ -4892,8 +4898,10 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_TILE_TITLE_VISIBILITY);
             PRIVATE_SETTINGS.add(QS_QUICKBAR_COLUMNS);
             PRIVATE_SETTINGS.add(BACK_GESTURE_HEIGHT);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_TYPE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
         }
-
 
         /**
          * Whether to display the torch option in the power menu
